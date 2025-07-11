258b853302645269fe432a5fe69f89bbb289940a
sanjay <sanjay8195850041@gmail.com>
2025-07-11 12:21:17 +0530

    add the ref hook

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -30,24 +30,28 @@ export default function Player() {
 // how to use useRef hook in react  
 
 
-import { useState , useRef } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
+  const [enterRedPlayer, setEnterRedPlayer] = useState("");
+  const playerName = useRef();
+  console.log("testPlayerName", playerName);
 
-  const [enterRedPlayer , setEnterRedPlayer] = useState("");
-  const [submited , setSubmited] = useState(false)
 
-  function handleChange(event) {
-    setEnterRedPlayer(event.target.value)
-  }
   function handleChick() {
-    setSubmited(true);
+    setEnterRedPlayer(playerName.current.value)
+
+    // this is clear the the code in react empty the input fild but this is not good code 
+    playerName.current.value = "";
   }
   return (
     <section id="player">
-      <h2>Welcome {submited ? enterRedPlayer : "unknown entity"}</h2>
+      <h2>Welcome {enterRedPlayer ?? "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enterRedPlayer}/>
+        <input
+          ref={playerName}
+          type="text"
+        />
         <button onClick={handleChick}>Set Name</button>
       </p>
     </section>
