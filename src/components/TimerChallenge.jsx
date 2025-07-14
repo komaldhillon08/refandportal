4c7778f62a0631c448e6df5f0f1115c112cfeb57
sanjay <sanjay8195850041@gmail.com>
2025-07-14 18:53:59 +0530

    all code use the useref

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,43 +2,31 @@ import { useRef, useState } from "react"
 import ResultModal from "./ResultModal.jsx";
 
 export default function TimerChallenge({ title, targetTime }) {
-
     // let timer;
     let timer = useRef();
-
     // how to use ref 
     let dialog = useRef();
-
     const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
-
     const timerIsAvtive = timeRemaining > 0 && timeRemaining < targetTime * 1000
-
     if (timeRemaining <= 0) {
         clearInterval(timer.current)
         dialog.current.open();
     }
-
     function handleRest() {
         setTimeRemaining(targetTime * 1000)
-        
     }
-
     // setTimeOut function 
     function handleStart() {
         timer.current = setInterval(() => {
             setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10)
-
         }, 10)
-
     }
     function handleStop() {
         dialog.current.open();
         clearInterval(timer)
     }
-
     return (
         <>
-
             {/* {timerExpired && <ResultModal ref={dialog} targetTime={targetTime} result="lost" />} */}
             <ResultModal
                 ref={dialog}
